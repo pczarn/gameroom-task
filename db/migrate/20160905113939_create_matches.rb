356cd080aa6c8148e1a1db13680bf7c88dfabcce
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-05 14:49:45 +0200

    Initialize 4 models with factories

---
+++ db/migrate/20160905113939_create_matches.rb
@@ -0,0 +1,14 @@
+class CreateMatches < ActiveRecord::Migration[5.0]
+  def change
+    create_table :matches do |t|
+      t.time :played_at
+      t.references :game, foreign_key: true
+      t.references :team_one, references: :team, foreign_key: true
+      t.references :team_two, references: :team, foreign_key: true
+      t.integer :team_one_score, null: true
+      t.integer :team_two_score, null: true
+
+      t.timestamps
+    end
+  end
+end
