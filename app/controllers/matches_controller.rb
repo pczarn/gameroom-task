46cd3b3f566fba964f7fcf5cf79df048aae3e131
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-09 16:45:45 +0200

    Add matches controller

---
+++ app/controllers/matches_controller.rb
@@ -0,0 +1,40 @@
+class MatchesController < ApplicationController
+  def create
+    @match = Match.create(match_params)
+    if @match.save
+      redirect_to @match
+    else
+      redirect_to action: :index
+    end
+  end
+
+  def index
+    @new_match = Match.new
+    @recent = Match.order(played_at: :desc).limit(10)
+    @game_ids_names = Game.all.pluck(:id, :name)
+    @team_ids_names = Team.all.pluck(:id, :name)
+  end
+
+  def show
+    @match = Match.find(params[:id])
+    @game_ids_names = Game.all.pluck(:id, :name)
+    @team_ids_names = Team.all.pluck(:id, :name)
+  end
+
+  def update
+    match = Match.find(params[:id])
+    match.update!(match_params)
+    redirect_to match
+  end
+
+  def destroy
+    Match.find(params[:id]).destroy
+    redirect_to action: :index
+  end
+
+  private
+
+  def match_params
+    params.require(:match).permit(:played_at, :game_id, :team_one_id, :team_two_id, :team_one_score, :team_two_score)
+  end
+end
