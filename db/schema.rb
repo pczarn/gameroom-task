84390399b07a91d665ee50afc5355adb10562bd1
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-09 11:20:58 +0200

    Initialize 4 models with factories
    Models have with basic associations and fields. There is a join table
    `UserTeams`, because teams will have many users and teams will have many users.
    Matches have two foreign keys that point to teams, so that teams can be
    reused in different matches. Rspec is configured to work with FactoryGirl.
    Factories are not written yet.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160905113958) do
+
+  create_table "games", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "matches", force: :cascade do |t|
+    t.time     "played_at"
+    t.integer  "game_id"
+    t.integer  "team_one_id"
+    t.integer  "team_two_id"
+    t.integer  "team_one_score"
+    t.integer  "team_two_score"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["game_id"], name: "index_matches_on_game_id"
+    t.index ["team_one_id"], name: "index_matches_on_team_one_id"
+    t.index ["team_two_id"], name: "index_matches_on_team_two_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_teams", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id"], name: "index_user_teams_on_team_id"
+    t.index ["user_id"], name: "index_user_teams_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
