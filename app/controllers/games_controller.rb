7b3cd539ecf132070080d96bbada5d7285af7d8b
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-13 09:14:54 +0200

    Create empty actions and views

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,19 @@
 class GamesController < ApplicationController
+  def create
+    redirect_to action: :index
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def update
+    redirect_to action: :index
+  end
+
+  def destroy
+    redirect_to action: :index
+  end
 end
