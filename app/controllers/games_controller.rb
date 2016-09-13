ce7bcc4a94679352494299f25e6f593203b41631
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-13 09:48:53 +0200

    Implement actions and views for Games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,19 +1,42 @@
 class GamesController < ApplicationController
   def create
-    redirect_to action: :index
+    game = Game.create(game_params)
+    if game.save
+      redirect_to game
+    else
+      flash[:error] = game.errors.full_messages
+      redirect_to action: :index
+    end
   end
 
   def index
+    @games = Game.page(params[:page])
+    @new_game = Game.new
   end
 
   def show
+    @game = Game.find(params[:id])
   end
 
   def update
+    game = Game.find(params[:id])
+    flash[:error] = game.errors.full_messages unless game.update(game_params)
     redirect_to action: :index
   end
 
   def destroy
+    game = Game.find(params[:id])
+    game.destroy
+    flash[:success] = "Game deleted"
     redirect_to action: :index
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(
+      :name,
+      image: [],
+    )
   end
 end
