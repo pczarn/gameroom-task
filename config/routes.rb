5d739bd4d8e5786d3770822834247309d3463133
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-14 16:20:09 +0200

    Revert change of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/matches/:id", to: "matches#edit", as: "match"
-  resources :matches, except: [:edit, :show]
+  resources :matches, except: [:show]
 end
