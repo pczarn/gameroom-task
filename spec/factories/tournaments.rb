a0fe2829fbe9a60dad570122d4debafc1b564891
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-15 10:52:14 +0200

    Write spec

--- spec/factories/tournaments.rb
+++ spec/factories/tournaments.rb
@@ -1,8 +1,24 @@
 FactoryGirl.define do
   factory :tournament do
-    title "MyString"
-    number_of_teams 1
-    started_at "2016-09-15 09:58:05"
-    number_of_members_per_team 1
+    title { "Tournament #{Faker::Space.star_cluster}"  }
+    number_of_teams { Faker::Number.between(2, 20) * 2 }
+    started_at { Faker::Time.between(1.year.ago, 1.year.from_now, :all) }
+
+    after(:build) do |tournament, evaluator|
+      if tournament.matches.empty?
+        tournament.matches << build_list(:match, evaluator.number_of_teams / 2)
+      end
+    end
+
+    factory :tournament_with_number_of_members do
+      number_of_members_per_team { Faker::Number.between(1, 30) }
+
+      after(:build) do |tournament, evaluator|
+        teams = tournament.matches.flat_map { |match| [match.team_one, match.team_two] }
+        teams.each do |team|
+          team.members = build_list(:user, evaluator.number_of_members_per_team)
+        end
+      end
+    end
   end
 end
--- spec/models/tournament_spec.rb
+++ spec/models/tournament_spec.rb
@@ -1,5 +1,92 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Tournament, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    subject { build(:tournament) }
+
+    describe "#matches" do
+      let(:tournament) { create(:tournament) }
+
+      context "with no matches" do
+        before { tournament.matches = [] }
+
+        it { is_expected.not_to be_invalid }
+      end
+
+      context "with one match" do
+        let(:match) { create(:match) }
+        let(:tournament) { build(:tournament, matches: [match]) }
+
+        it { is_expected.not_to be_valid }
+      end
+    end
+
+    describe "#title" do
+      context "with title not present" do
+        let(:tournament) { build(:tournament, title: "") }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context "with title present" do
+        let(:tournament) { build(:tournament, title: "Test") }
+
+        it { is_expected.to be_valid }
+      end
+
+      context "when not unique" do
+        let(:title) { "Title" }
+        before { create(:tournament, title: title) }
+        let(:tournament) { build(:tournament, title: title) }
+
+        it { is_expected.to be_valid }
+      end
+    end
+
+    describe "#number_of_teams" do
+      let(:tournament) { build(:tournament, number_of_teams: number_of_teams) }
+
+      context "with no teams" do
+        let(:number_of_teams) { 1 }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context "with one team" do
+        let(:number_of_teams) { 1 }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context "with two teams" do
+        let(:number_of_teams) { 1 }
+
+        it { is_expected.to be_valid }
+      end
+    end
+
+    describe "#number_of_members_per_team" do
+      let(:tournament) { build(:tournament, number_of_members_per_team: number_of_members) }
+
+      context "with no members" do
+        let(:number_of_members) { 0 }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context "with one member per team" do
+        let(:number_of_members) { 1 }
+
+        it { is_expected.to be_valid }
+      end
+    end
+  end
+
+  describe "#started_at" do
+    let(:tournament) { create(:tournament) }
+
+    it "is a time" do
+      expect(tournament.started_at).to be_a(Time)
+    end
+  end
 end
