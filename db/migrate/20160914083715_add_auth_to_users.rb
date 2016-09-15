5aea028ea4fbd10209c288a4adacc24c6e2cbb84
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-15 09:14:56 +0200

    Require email and password_hashed

--- db/migrate/20160914083715_add_auth_to_users.rb
+++ db/migrate/20160914083715_add_auth_to_users.rb
@@ -1,7 +1,7 @@
 class AddAuthToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :email, :string
-    add_column :users, :password_hashed, :string
+    add_column :users, :email, :string, null: false
+    add_column :users, :password_hashed, :string, null: false
     add_index :users, :email, unique: true
   end
 end
