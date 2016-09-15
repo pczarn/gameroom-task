9082bde249fccbb7a18e4ff3785e19a016e11fa7
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-15 09:16:08 +0200

    Add column for user role

--- db/migrate/20160914083715_add_auth_to_users.rb
+++ db/migrate/20160914083715_add_auth_to_users.rb
@@ -2,6 +2,7 @@ class AddAuthToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :email, :string, null: false
     add_column :users, :password_hashed, :string, null: false
+    add_column :users, :role, :integer, null: false, default: 0
     add_index :users, :email, unique: true
   end
 end
