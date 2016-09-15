545a99c1b1c123f10568bf76b577168b39b9542a
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-15 16:26:59 +0200

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get "log_in" => "sessions#new", as: "log_in"
-  delete "log_out" => "sessions#destroy", as: "log_out"
-  get "sign_up" => "users#new", as: "sign_up"
-
   root "sessions#new"
 
   resources :users, only: [:create]
-  resources :sessions, only: [:create]
+  get "sign_up" => "users#new", as: "sign_up"
+
+  resource :sessions, only: [:create]
+  get "log_in" => "sessions#new", as: "log_in"
+  delete "log_out" => "sessions#destroy", as: "log_out"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :matches, except: [:show]
