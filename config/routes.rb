832cd828b673b68d3a769d4feccdb00f2b3616f0
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-15 16:37:31 +0200

    Change syntax

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   root "sessions#new"
 
   resources :users, only: [:create]
-  get "sign_up" => "users#new", as: "sign_up"
+  get { "sign_up" => "users#new" }, as: "sign_up"
 
   resource :sessions, only: [:create]
-  get "log_in" => "sessions#new", as: "log_in"
-  delete "log_out" => "sessions#destroy", as: "log_out"
+  get { "log_in" => "sessions#new" }, as: "log_in"
+  delete { "log_out" => "sessions#destroy" }, as: "log_out"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :matches, except: [:show]
