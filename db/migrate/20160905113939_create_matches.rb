6e4875d269cb6912356c36cdc038bbaa128a43ad
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-16 16:51:44 +0200

    Revert change of Match migration

--- db/migrate/20160905113939_create_matches.rb
+++ db/migrate/20160905113939_create_matches.rb
@@ -1,7 +1,7 @@
 class CreateMatches < ActiveRecord::Migration[5.0]
   def change
     create_table :matches do |t|
-      t.datetime :played_at
+      t.datetime :played_at, null: false
       t.references :game, foreign_key: true, null: false
       t.references :team_one, references: :team, null: false
       t.references :team_two, references: :team, null: false
