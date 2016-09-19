9bf6ec82a38b500733f03aa5cc0e46aaeda750df
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-19 13:23:22 +0200

    Fix factory for rounds

--- spec/factories/matches.rb
+++ spec/factories/matches.rb
@@ -1,7 +1,11 @@
 FactoryGirl.define do
   factory :match do
+    transient do
+      played_after 1.year.ago
+    end
+
     game
-    played_at { Faker::Time.between(1.year.ago, Time.zone.today, :all) }
+    played_at { Faker::Time.between(played_after, played_after + 1.year, :all) }
     association :team_one, factory: :team
     association :team_two, factory: :team
     team_one_score { Faker::Number.between(0, 100) }
