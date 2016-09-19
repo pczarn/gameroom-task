b43919328d66740b3aa66a375503dada65b194c9
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-19 12:00:38 +0200

    Add a separate migration UpdateMatches

--- db/migrate/20160916101106_create_rounds.rb
+++ db/migrate/20160916101106_create_rounds.rb
@@ -7,8 +7,6 @@ class CreateRounds < ActiveRecord::Migration[5.0]
       t.timestamps
     end
 
-    add_reference :matches, :round, foreign_key: true
-    change_column_null :matches, :played_at, true
     add_index :rounds, [:tournament_id, :number], unique: true
   end
 end
