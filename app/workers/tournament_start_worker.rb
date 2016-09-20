4cb35770956351a0b719b5d8ac9a2b702fbcb026
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-20 16:02:58 +0200

    Refactor

--- app/workers/tournament_start_worker.rb
+++ app/workers/tournament_start_worker.rb
@@ -3,25 +3,20 @@ class TournamentStartWorker
 
   def perform(tournament_id, performed_at)
     tournament = Tournament.find(tournament_id)
-    if tournament.started_at == performed_at
-      if tournament.can_be_started?
-        start(tournament)
-      else
-        TournamentStartWorker.perform_in(1.day, tournament_id, performed_at)
-      end
-    end
+    # start(tournament) if ready_to_start?(tournament, performed_at)
+    try_start(tournament, performed_at) if tournament.started_at == performed_at
   end
 
-  def ready_to_start?(tournament, performed_at)
-    tournament.started_at == performed_at && tournament.can_be_started?
+  def try_start(tournament, performed_at)
+    if tournament.can_be_started?
+      start(tournament)
+    else
+      TournamentStartWorker.perform_in(1.day, tournament.id, performed_at)
+    end
   end
 
   def start(tournament)
-    round = tournament.rounds.build(number: 0)
-    tournament.teams.each_slice(2) do |team_pair|
-      team_one, team_two = *team_pair
-      round.matches.build(game: tournament.game, team_one: team_one, team_two: team_two)
-    end
+    tournament.build_first_round
     tournament.started!
     tournament.save!
     notify_members_about_start(tournament)
