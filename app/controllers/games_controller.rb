b7e5bbb8bc91e7f31abd21f6ceef0e49c1e16f45
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-21 09:41:54 +0200

    Fix controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,37 +1,45 @@
 class GamesController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :load_game, only: [:edit, :update, :destroy]
+
   def create
-    game = Game.create(game_params)
-    if game.save
-      redirect_to game
+    @game = Game.create(game_params)
+    if @game.save
+      redirect_to edit_game_path(@game)
     else
       flash[:error] = game.errors.full_messages
-      redirect_to action: :index
+      render "index"
     end
   end
 
   def index
     @games = Game.page(params[:page])
-    @new_game = Game.new
+    @game = Game.new
   end
 
-  def show
-    @game = Game.find(params[:id])
+  def edit
   end
 
   def update
-    game = Game.find(params[:id])
-    flash[:error] = game.errors.full_messages unless game.update(game_params)
-    redirect_to action: :index
+    if @game.update(game_params)
+      redirect_to edit_game_path(@game)
+    else
+      flash.now.alert = @game.errors.full_messages.to_sentence
+      render "edit"
+    end
   end
 
   def destroy
-    game = Game.find(params[:id])
-    game.destroy
+    @game.destroy
     flash[:success] = "Game deleted"
     redirect_to action: :index
   end
 
   private
+
+  def load_game
+    @game = Game.find(params[:id])
+  end
 
   def game_params
     params.require(:game).permit(
