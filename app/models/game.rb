4ff3f8edb1c88dad9e1a637cc2aad52a1cf1eb3c
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-21 14:44:53 +0200

    Add enum for game state, active or archivized

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,6 +4,11 @@ class Game < ApplicationRecord
   has_many :matches
   has_many :tournaments
 
+  enum state_archivized: {
+    archivized: true,
+    active: false,
+  }
+
   validates :name, presence: true, uniqueness: true
 
   mount_uploader :image, ImageUploader
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919134326) do
+ActiveRecord::Schema.define(version: 20160921115742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "games", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",                             null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.string   "image"
+    t.boolean  "state_archivized", default: false, null: false
     t.index ["name"], name: "index_games_on_name", unique: true, using: :btree
   end
 
