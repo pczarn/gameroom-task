829a8ffb3cd17a54b3319cb81f7775c482c47d6e
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-22 14:57:09 +0200

    Fix controller helper

--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -4,7 +4,7 @@ module ControllerHelpers
     allow(controller).to receive(:current_user).and_return(user)
   end
 
-  def sign_in_admin(user = double("user"))
+  def sign_in_admin(user = build(:user))
     allow(user).to receive(:admin?).and_return(true)
     allow(controller).to receive(:authenticate!)
     allow(controller).to receive(:current_user).and_return(user)
