437f8749422a18802d43b78d125b988deea48550
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-23 16:16:12 +0200

    Add ownership of teams for users

--- app/models/user_team.rb
+++ app/models/user_team.rb
@@ -2,5 +2,10 @@ class UserTeam < ApplicationRecord
   belongs_to :user
   belongs_to :team
 
+  enum role: {
+    regular: 0,
+    owner: 1,
+  }
+
   validates :user_id, uniqueness: { scope: :team_id }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919134326) do
+ActiveRecord::Schema.define(version: 20160923122629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,10 +84,11 @@ ActiveRecord::Schema.define(version: 20160919134326) do
   end
 
   create_table "user_teams", force: :cascade do |t|
-    t.integer  "user_id",    null: false
-    t.integer  "team_id",    null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "user_id",                null: false
+    t.integer  "team_id",                null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "role",       default: 0
     t.index ["team_id"], name: "index_user_teams_on_team_id", using: :btree
     t.index ["user_id", "team_id"], name: "index_user_teams_on_user_id_and_team_id", unique: true, using: :btree
     t.index ["user_id"], name: "index_user_teams_on_user_id", using: :btree
