742600dfd0268cdfa4ecb74145bc5d7da5eeaa71
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-26 13:54:37 +0200

    Add spec

--- spec/helpers/teams_helper_spec.rb
+++ spec/helpers/teams_helper_spec.rb
@@ -2,6 +2,23 @@ require "rails_helper"
 
 RSpec.describe TeamsHelper, type: :helper do
   describe "joining_user_ids_and_names" do
-    # TODO
+    let(:team) { build(:team) }
+
+    context "with the user not in the team" do
+      before { create(:user) }
+
+      it { expect(joining_user_ids_and_names(team).size).to eq(1) }
+    end
+
+    context "with the user in the team" do
+      let(:user) { create(:user) }
+
+      before do
+        team.members << user
+        team.save!
+      end
+
+      it { expect(joining_user_ids_and_names(team).size).to eq(0) }
+    end
   end
 end
