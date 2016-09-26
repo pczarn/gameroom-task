bd30623f6a47e6cbcc4219abe43297da86dc7207
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-26 09:42:04 +0200

    Validate limit of members per team

--- app/models/team_tournament.rb
+++ app/models/team_tournament.rb
@@ -8,9 +8,17 @@ class TeamTournament < ApplicationRecord
     !size_less_than?(tournament.number_of_members_per_team) || !size_less_than?(team_size_limit)
   end
 
+  def overfull?
+    !size_within?(tournament.number_of_members_per_team) || !size_within?(team_size_limit)
+  end
+
   private
 
   def size_less_than?(limit)
     limit.nil? || team.members.count < limit
+  end
+
+  def size_within?(limit)
+    limit.nil? || team.members.count <= limit
   end
 end
