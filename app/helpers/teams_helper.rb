807e89e5bf1dea2b899868e42faacc38fd39af18
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-26 14:08:28 +0200

    Rename method

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,5 +1,5 @@
 module TeamsHelper
-  def joining_user_ids_and_names(team)
+  def potential_member_ids_and_names(team)
     User.pluck(:id, :name) - User.joins(:teams).where(teams: { id: team.id }).pluck(:id, :name)
   end
 end
--- spec/helpers/teams_helper_spec.rb
+++ spec/helpers/teams_helper_spec.rb
@@ -1,13 +1,15 @@
 require "rails_helper"
 
 RSpec.describe TeamsHelper, type: :helper do
-  describe "joining_user_ids_and_names" do
+  describe "#potential_member_ids_and_names" do
     let(:team) { build(:team) }
 
     context "with the user not in the team" do
       before { create(:user) }
 
-      it { expect(joining_user_ids_and_names(team).size).to eq(1) }
+      it "returns a potential user" do
+        expect(potential_member_ids_and_names(team).size).to eq(1)
+      end
     end
 
     context "with the user in the team" do
@@ -18,7 +20,9 @@ RSpec.describe TeamsHelper, type: :helper do
         team.save!
       end
 
-      it { expect(joining_user_ids_and_names(team).size).to eq(0) }
+      it "returns no potential users" do
+        expect(potential_member_ids_and_names(team).size).to eq(0)
+      end
     end
   end
 end
