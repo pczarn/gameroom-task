1585ef61ff9fc133ec3137fe54bc3c9e2e1ca06b
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-27 18:22:39 +0200

    Finish spec

--- spec/workers/tournament_start_worker_spec.rb
+++ spec/workers/tournament_start_worker_spec.rb
@@ -1,50 +1,66 @@
 require "rails_helper"
 
 RSpec.describe TournamentStartWorker, type: :worker do
-  subject(:perform) { worker.perform(tournament.id, tournament.started_at) }
-  let(:worker) { TournamentStartWorker.new }
+  describe "#perform" do
+    subject(:perform) { worker.perform(tournament.id, tournament.started_at) }
+    let(:worker) { TournamentStartWorker.new }
+    let(:tournament) { create(:tournament, number_of_teams: number_of_teams) }
+    let(:number_of_teams) { 8 }
 
-  context "when tournament is ready" do
-    let(:tournament) { create(:tournament) }
+    context "when tournament is ready" do
+      it "starts" do
+        expect(worker).to receive(:start)
+        perform
+      end
 
-    it "starts" do
-      # why does rubocop give a warning to use 'allow'?
-      expect(worker).to receive(:start)
-      perform
+      it "sets the status" do
+        expect { perform }.to change { tournament.reload.started? }.to be(true)
+      end
+
+      it "creates the first round" do
+        expect { perform }.to change { tournament.reload.rounds.size }.from(0).to(1)
+      end
+
+      it "creates a round with a correct number of matches" do
+        perform
+        tournament.reload
+        expect(tournament.matches.count).to eq(number_of_teams / 2)
+      end
+
+      it "notifies players" do
+        expect(UserMailer).to receive(:notify_about_tournament_start).at_least(:once)
+        perform
+      end
     end
 
-    it "sets the status" do
-      expect { perform }.to change { tournament.reload.started? }.to be(true)
+    context "when tournament is not ready" do
+      before do
+        allow(Tournament).to receive(:find).and_return(tournament)
+        allow(tournament).to receive(:can_be_started?).and_return(false)
+      end
+
+      it "does not start" do
+        expect(worker).not_to receive(:start)
+        perform
+      end
+
+      it "does not change status" do
+        expect { perform }.not_to change { tournament.reload.open? }
+      end
+
+      it "adds a new job" do
+        expect(TournamentStartWorker).to receive(:perform_in)
+        perform
+      end
     end
 
-    it "creates the first round" do
-      expect { perform }.to change { tournament.reload.rounds.size }.from(0).to(1)
-    end
+    context "when time does not match the updated time" do
+      subject(:perform) { worker.perform(tournament.id, tournament.started_at - 1.day) }
 
-    it "creates a round with a correct number of matches" do
-      perform
-      tournament.reload
-      # expect(Round).to receive(:create) with params number:0...
-      expect(tournament.rounds.find_by_number(0).matches.size).to eq(tournament.teams.size / 2)
-    end
-  end
-
-  context "when tournament is not ready" do
-    let(:tournament) { create(:tournament, number_of_members_per_team: 666) }
-
-    it "does not start" do
-      expect(worker).not_to receive(:start)
-      perform
-    end
-
-    it "does not change status" do
-      expect { perform }.not_to change { tournament.reload.open? }
-    end
-  end
-
-  context "when time does not match the updated time" do
-    it "does not run twice" do
-      # TournamentStartWorker.perform_at()
+      it "does not start" do
+        expect(worker).not_to receive(:try_start)
+        perform
+      end
     end
   end
 end
