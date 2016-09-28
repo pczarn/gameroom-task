70e369d3b71b848754dfac5494c4d487d8ab61fb
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-28 12:35:55 +0200

    Implement viewing rounds

--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -6,4 +6,18 @@ module TournamentsHelper
   def potential_team_names_and_ids(tournament)
     tournament.potential_teams.pluck(:name, :id)
   end
+
+  def editable?(match, user)
+    user.admin? || scores_not_set?(match) && owned_by?(match, user)
+  end
+
+  def scores_not_set?(match)
+    match.team_one_score.nil? || match.team_two_score.nil?
+  end
+
+  def owned_by?(match, user)
+    match.round.tournament.owner.id == user.id ||
+      match.team_one.member_ids.include?(user.id) ||
+      match.team_two.member_ids.include?(user.id)
+  end
 end
