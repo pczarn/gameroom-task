43b14f462d705f33d6afed2fdbb40b08b058fec3
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-29 18:15:03 +0200

    Add spec for mailer

--- spec/mailers/tournament_status_mailer_spec.rb
+++ spec/mailers/tournament_status_mailer_spec.rb
@@ -1,47 +1,46 @@
 require "rails_helper"
 
-RSpec.describe TournamentStatusMailer, type: :mailer do
-  describe ".notify_about_start" do
-    subject(:mail) { TournamentStatusMailer.notify_about_start(user.id, tournament.id) }
-    let(:user) { create(:user) }
-    let(:tournament) { create(:tournament) }
+shared_examples_for "mail_sender" do
+  let(:user) { create(:user) }
 
-    it "delivers successfully" do
-      expect { mail.deliver }.not_to raise_error
-    end
-
-    describe "#to" do
-      it { expect(mail.to).to eq([user.email]) }
-    end
-
-    describe "#subject" do
-      it { expect(mail.subject).to be_present }
-    end
-
-    describe "#body" do
-      it { expect(mail.body).to be_present }
-    end
+  it "delivers successfully" do
+    expect { mail.deliver }.not_to raise_error
   end
 
-  # describe ".notify_about_start" do
-  #   subject(:mail) { TournamentStatusMailer.notify_about_start(user.id, tournament.id) }
-  #   let(:user) { create(:user) }
-  #   let(:tournament) { create(:tournament) }
+  describe "#to" do
+    it { expect(mail.to).to eq([user.email]) }
+  end
 
-  #   it "delivers successfully" do
-  #     expect { mail.deliver }.not_to raise_error
-  #   end
+  describe "#subject" do
+    it { expect(mail.subject).to be_present }
+  end
 
-  #   describe "#to" do
-  #     it { expect(mail.to).to eq([user.email]) }
-  #   end
+  describe "#body" do
+    it { expect(mail.body).to be_present }
+  end
+end
 
-  #   describe "#subject" do
-  #     it { expect(mail.subject).to be_present }
-  #   end
+RSpec.describe TournamentStatusMailer, type: :mailer do
+  let(:tournament) { create(:tournament) }
 
-  #   describe "#body" do
-  #     it { expect(mail.body).to be_present }
-  #   end
-  # end
+  describe ".notify_about_start" do
+    subject(:mail) { TournamentStatusMailer.notify_about_start(user.id, tournament.id) }
+
+    it_behaves_like "mail_sender"
+  end
+
+  describe ".notify_about_end" do
+    subject(:mail) { TournamentStatusMailer.notify_about_end(user.id, tournament.id, team.id) }
+    let(:team) { create(:team) }
+
+    it_behaves_like "mail_sender"
+  end
+
+  describe ".notify_about_match_result" do
+    subject(:mail) { TournamentStatusMailer.notify_about_match_result(user.id, match.id) }
+    let(:round) { create(:round, tournament: tournament) }
+    let(:match) { create(:match, round: round, played_after: tournament.started_at) }
+
+    it_behaves_like "mail_sender"
+  end
 end
