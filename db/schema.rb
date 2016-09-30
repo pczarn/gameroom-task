f9eaa21afae20eb5691ec7deaeb26af46a1b485d
Piotr Czarnecki <pioczarn@gmail.com>
2016-09-30 14:23:30 +0200

    Remove uniqueness index on team names

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930071221) do
+ActiveRecord::Schema.define(version: 20160930093639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,7 +66,6 @@ ActiveRecord::Schema.define(version: 20160930071221) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_teams_on_name", unique: true, using: :btree
   end
 
   create_table "tournaments", force: :cascade do |t|
