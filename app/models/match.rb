3847ceedf986a23471aa4897bd9156af7cf1ce78
Piotr Czarnecki <pioczarn@gmail.com>
2016-10-10 16:07:34 +0200

    Refactor the Match model

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,9 +1,13 @@
+require "delegate"
+
 class Match < ApplicationRecord
   belongs_to :game
   belongs_to :team_one, class_name: Team
   belongs_to :team_two, class_name: Team
   belongs_to :round
   belongs_to :owner, class_name: User, inverse_of: :owned_matches
+
+  delegate :tournament, to: :round
 
   scope :friendly, -> { where(round: nil) }
 
@@ -20,25 +24,15 @@ class Match < ApplicationRecord
   validate :no_repeated_members_across_teams, :played_after_tournament_start
 
   def winning_team
-    @winning_team ||= teams_in_order[0]
+    teams_ordered_by_score[0]
   end
 
   def defeated_team
-    @defeated_team ||= teams_in_order[1]
+    teams_ordered_by_score[1]
   end
 
-  def teams_in_order
-    @teams_in_order ||= begin
-      return teams unless team_one_score && team_two_score
-      team_one_score < team_two_score ? teams.reverse : teams
-    end
-  end
-
-  def teams
-    @teams ||= [
-      TeamInMatch.new(team_one.id, team_one.name, team_one_score),
-      TeamInMatch.new(team_two.id, team_two.name, team_two_score),
-    ]
+  def teams_ordered_by_score
+    @teams_ordered_by_score ||= teams.sort
   end
 
   def scores
@@ -57,10 +51,30 @@ class Match < ApplicationRecord
   end
 
   def played_after_tournament_start
-    if played_at && round && played_at < round.tournament.started_at
+    if played_at && round && played_at < tournament.started_at
       errors.add(:played_at, "Can't be played before the tournament starts")
     end
   end
+
+  def teams
+    @teams ||= [
+      TeamInMatchDecorator.new(team_one, score: team_one_score),
+      TeamInMatchDecorator.new(team_two, score: team_two_score),
+    ]
+  end
 end
 
-TeamInMatch = Struct.new(:id, :name, :score)
+class TeamInMatchDecorator < SimpleDelegator
+  include Comparable
+
+  attr_reader :score
+
+  def initialize(team, score:)
+    @score = score
+    super(team)
+  end
+
+  def <=>(other)
+    (other.score || -1) <=> (@score || -1)
+  end
+end
