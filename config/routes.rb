88022969ce0ccdf4361866e6825f60d92e5d8b82
Piotr Czarnecki <pioczarn@gmail.com>
2016-10-17 15:59:17 +0200

    Remove the route for sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,6 @@ Rails.application.routes.draw do
 
       resources :users, only: [:create, :show, :update]
 
-      resource :sessions, only: :create
-      delete "log_out", to: "sessions#destroy", as: "log_out"
-
       resources :teams, only: [:create, :index, :show, :update]
 
       resources :matches, only: [:create, :index, :show, :update, :destroy]
