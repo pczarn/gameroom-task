30bffb966c068496d0a6654e72cdb051754d60c6
Piotr Czarnecki <pioczarn@gmail.com>
2016-10-17 16:03:39 +0200

    Remove views

--- app/helpers/tournaments_helper.rb
+++
@@ -1,9 +0,0 @@
-module TournamentsHelper
-  def potential_teams_for_select(tournament)
-    options_for_select(potential_team_names_and_ids(tournament))
-  end
-
-  def potential_team_names_and_ids(tournament)
-    tournament.potential_teams.pluck(:name, :id)
-  end
-end
