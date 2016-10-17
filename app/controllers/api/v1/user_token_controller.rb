d702ace1ce27751306f14f4894473885939e910d
Piotr Czarnecki <pioczarn@gmail.com>
2016-10-17 16:39:41 +0200

    Add user info to user token response

--- app/controllers/api/v1/user_token_controller.rb
+++ app/controllers/api/v1/user_token_controller.rb
@@ -3,6 +3,10 @@ module Api
     User = User
 
     class UserTokenController < Knock::AuthTokenController
+      def create
+        render json: { knock: auth_token.to_json, user: UserRepresenter.new(entity) },
+               status: :created
+      end
     end
   end
 end
