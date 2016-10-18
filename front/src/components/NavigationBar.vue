0a83c0cda0e8f0bfa7c1d3f81d06f3e7b121970d
Piotr Czarnecki <pioczarn@gmail.com>
2016-10-18 16:23:30 +0200

    Add navigation bar component

---
+++ front/src/components/NavigationBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+
+      <li v-if="isLoggedIn">
+        Hi, {{ currentUser.name }} <br>
+        <router-link to="/logout">
+          Log out
+        </router-link>
+      </li>
+      <li v-else>
+        <router-link to="/login">Log in</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+
+  export default {
+    computed: mapGetters(['currentUser', 'isLoggedIn']),
+  }
+</script>
+
+<style scoped>
+</style>
