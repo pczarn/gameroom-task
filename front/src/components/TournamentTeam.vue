b28e9bed74e50a841c6470f77ddf77645a70d304
Piotr Czarnecki <pioczarn@gmail.com>
2016-10-20 11:33:49 +0200

    Fix tournaments

---
+++ front/src/components/TournamentTeam.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="team">
+  <strong>{{name}}</strong>
+  <team-member-list :members="members"></team-member-list>
+  <router-link :to="{ name: 'team', params: { id } }">See more</router-link>
+</div>
+</template>
+
+<script>
+import TeamMemberList from './TeamMemberList'
+
+export default {
+  name: 'TournamentTeam',
+  props: {
+    id: Number,
+    name: String,
+    members: Array,
+  },
+  components: {
+    TeamMemberList,
+  },
+  data () {
+    return {
+      editable: false,
+    }
+  },
+  computed: {
+  },
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
