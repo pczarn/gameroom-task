d4ad2d5e445aad2957adf47ba294c8043db763a9
Piotr Czarnecki <pioczarn@gmail.com>
2016-10-24 16:55:06 +0200

    Fixes

--- front/src/components/TeamForm.vue
+++ front/src/components/TeamForm.vue
@@ -11,12 +11,13 @@
         <!-- <vue-multiselect></vue-multiselect> -->
       </fieldset>
 
-      <button type="submit">{{ button }}</button>
+      <button type="submit">{{ buttonText }}</button>
     </form>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import { mapGetters } from 'vuex'
 
 export default {
@@ -24,6 +25,11 @@ export default {
   props: {
     value: Object,
     buttonText: String,
+  },
+  data () {
+    return {
+      team: Vue.util.extend({}, this.value),
+    }
   },
   computed: mapGetters(['userList']),
 }
