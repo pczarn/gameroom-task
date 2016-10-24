037ac8f063b03d23ff7281511c06f071583463c4
Piotr Czarnecki <pioczarn@gmail.com>
2016-10-24 14:01:45 +0200

    Work on games

--- front/src/components/GameList.vue
+++ front/src/components/GameList.vue
@@ -4,22 +4,14 @@
 
   <template v-if="isAdmin">
     <h2>Add a game</h2>
-    <form @submit.prevent>
-      <fieldset>
-        <legend>Properties</legend>
-        <template v-for="attrs in game_fields">
-          <label :for="attrs.name">{{attrs.name}}</label>
-          <input v-bind="attrs" v-model="new_game[attrs.name]">
-        </template>
-        <button type="submit" @click="create()">Add the game</button>
-      </fieldset>
-    </form>
+    <game-form :value="newGame" @submit="create"></game-form>
   </template>
 
   <h2>Active games</h2>
   <ul>
     <li v-for="game in activeGames">
-      <Game v-bind="game" :editable="isAdmin" @remove="destroy(game)">
+      <game-overview v-bind="game" :editable="isAdmin" @remove="destroy(game)">
+      </game-overview>
       <button v-if="isAdmin" @click="archivize(game)">Archivize</button>
     </li>
   </ul>
@@ -27,7 +19,8 @@
   <h2>Archivized games</h2>
   <ul>
     <li v-for="game in archivizedGames">
-      <Game v-bind="game" :editable="isAdmin" @remove="destroy(game)">
+      <game-overview v-bind="game" :editable="isAdmin" @remove="destroy(game)">
+      </game-overview>
       <button v-if="isAdmin" v-on:click="activate(game)">Activate</button>
     </li>
   </ul>
@@ -36,47 +29,39 @@
 
 <script>
 import { mapGetters } from 'vuex'
-import Game from './Game'
+import GameOverview from './GameOverview'
+import GameForm from './GameForm'
 
 export default {
+  components: {
+    GameOverview,
+    GameForm,
+  },
   data () {
     return {
-      game_fields: [
-        { name: 'name', type: 'text' },
-        { name: 'image', type: 'file', accept: "image/*" },
-      ],
-      new_game: {},
+      newGame: {},
     }
-  },
-  components: {
-    Game
-  },
-  beforeMount() {
-    this.$store.dispatch('GET_GAMES')
-  },
-  methods: {
-    create() {
-      this.$store.dispatch('CREATE_GAME', this.new_game)
-        .then(_ => { this.new_game = {} })
-    },
-    destroy(game) {
-      this.$store.dispatch('DESTROY_GAME', game)
-    },
-    archivize(game) {
-      this.setArchivized(game, true)
-    },
-    activate(game) {
-      this.setArchivized(game, false)
-    },
-    setArchivized(game, archivized) {
-      this.$store.dispatch('UPDATE_GAME', { state_archivized: archivized })
-    },
   },
   computed: {
     ...mapGetters(['isAdmin', 'activeGames', 'archivizedGames']),
-  }
+  },
+  methods: {
+    async create (game) {
+      await this.$store.dispatch('CREATE_GAME', game)
+      this.newGame = {}
+    },
+    destroy (game) {
+      this.$store.dispatch('DESTROY_GAME', game)
+    },
+    archivize (game) {
+      this.setArchivized(game, true)
+    },
+    activate (game) {
+      this.setArchivized(game, false)
+    },
+    setArchivized (game, archivized) {
+      this.$store.dispatch('UPDATE_GAME', { state_archivized: archivized })
+    },
+  },
 }
 </script>
-
-<style scoped>
-</style>
