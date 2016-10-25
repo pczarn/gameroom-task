a495a9522a80a9dd91cd14ed3e429bb7a5b36e48
Piotr Czarnecki <pioczarn@gmail.com>
2016-10-25 12:39:52 +0200

    Add joining and leaving tournaments

--- front/src/components/TournamentTeamList.vue
+++ front/src/components/TournamentTeamList.vue
@@ -1,22 +1,44 @@
 <template>
 <div>
-  <div v-for="team in teams">
+  <div v-for="team in tournament.teams">
     size {{ team.team_size_limit || 'no limit' }}
-    <tournament-team v-bind="team"></tournament-team>
+    <tournament-team-overview v-bind="team"></tournament-team-overview>
+    <button v-if="canJoin(team)" @click.prevent="join(team)">Join</button>
+    <button v-if="canLeave(team)" @click.prevent="leave(team)">Leave</button>
   </div>
 </div>
 </template>
 
 <script>
-import TournamentTeam from './TournamentTeam'
+import policies from 'src/policies'
+import TournamentTeamOverview from './TournamentTeamOverview'
 
 export default {
   props: {
-    teams: Array,
-    editable: Boolean,
+    tournament: Object,
+    joinable: Boolean,
   },
   components: {
-    TournamentTeam,
+    TournamentTeamOverview,
+  },
+  computed: {
+    canJoin (team) {
+      return policies.teamTournamentPolicy(this.tournament, team).join
+    },
+    canLeave (team) {
+      return policies.teamTournamentPolicy(this.tournament, team).leave
+    },
+  },
+  methods: {
+    join (team) {
+      team.member_ids.push(this.currentUser.id)
+      this.$store.dispatch('UPDATE_TOURNAMENT_LINEUP', [this.tournament, team])
+    },
+    leave (team) {
+      const idx = team.member_ids.findIndex(id => id === this.currentUser.id)
+      team.member_ids.splice(idx, 1)
+      this.$store.dispatch('UPDATE_TOURNAMENT_LINEUP', [this.tournament, team])
+    },
   },
 }
 </script>
