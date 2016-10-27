ef96c5eb9b594567e4417c2de139f6b061b62989
Piotr Czarnecki <pioczarn@gmail.com>
2016-10-27 16:41:56 +0200

    Add editing tournament members

--- front/src/components/TournamentTeamList.vue
+++ front/src/components/TournamentTeamList.vue
@@ -1,10 +1,14 @@
 <template>
 <div>
-  <div v-for="(team, idx) in tournament.teams">
+  <div v-for="(team, idx) in teams">
     size {{ team.team_size_limit || 'no limit' }}
-    <tournament-team-overview v-bind="team"></tournament-team-overview>
-    <button v-if="canJoin[idx]" @click.prevent="join(team)">Join</button>
-    <button v-if="canLeave[idx]" @click.prevent="leave(team)">Leave</button>
+    <tournament-team-overview v-bind="team"
+                              :tournament="tournament"
+                              @join="join"
+                              @leave="leave"
+                              @add-member="addMember(team, $event)"
+                              @remove-member="removeMember(team, $event)">
+    </tournament-team-overview>
   </div>
 </div>
 </template>
@@ -12,26 +16,43 @@
 <script>
 import policies from 'src/policies'
 import TournamentTeamOverview from './TournamentTeamOverview'
+// import { mapGetters } from 'vuex'
+import _ from 'lodash'
 
 export default {
   props: {
     tournament: Object,
-    joinable: Boolean,
+    // joinable: Boolean,
+    editable: Boolean,
   },
   components: {
     TournamentTeamOverview,
+    // Multiselect,
   },
+  // data () {
+  //   return {
+  //     teams: this.tournament.teams.map(team => {
+  //       team = _.cloneDeep(team)
+  //       team.policy = policies.teamTournamentPolicy(this.tournament, team)
+  //       return team
+  //     })
+  //   }
+  // },
   computed: {
-    canJoin () {
+    teams () {
       return this.tournament.teams.map(team => {
-        return policies.teamTournamentPolicy(this.tournament, team).join
+        team = _.clone(team)
+        team.policy = policies.teamTournamentPolicy(this.tournament, team)
+        return team
       })
     },
-    canLeave () {
-      return this.tournament.teams.map(team => {
-        return policies.teamTournamentPolicy(this.tournament, team).leave
-      })
-    },
+    // teams () {
+    //   for(const team of this.tournament.teams) {
+    //     team.policy = policies.teamTournamentPolicy(this.tournament, team)
+    //   }
+    //   return this.tournament.teams
+    // },
+    // ...mapGetters(['userList']),
   },
   methods: {
     join (team) {
@@ -43,9 +64,17 @@ export default {
       team.members.splice(idx, 1)
       this.$store.dispatch('UPDATE_TOURNAMENT_LINEUP', [this.tournament, team])
     },
+    addMember (team, member) {
+      const newTeam = _.cloneDeep(team)
+      newTeam.members.push(member)
+      this.$store.dispatch('UPDATE_TOURNAMENT_LINEUP', [this.tournament, newTeam])
+    },
+    removeMember (team, member) {
+      const newTeam = _.cloneDeep(team)
+      const idx = newTeam.members.findIndex(m => m.id === member.id)
+      newTeam.members.splice(idx, 1)
+      this.$store.dispatch('UPDATE_TOURNAMENT_LINEUP', [this.tournament, newTeam])
+    },
   },
 }
 </script>
-
-<style scoped>
-</style>
