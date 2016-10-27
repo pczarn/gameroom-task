83dc3b26deb0a9747b0a74b740f882d4eee29b60
Piotr Czarnecki <pioczarn@gmail.com>
2016-10-27 16:41:56 +0200

    Add editing tournament members

--- front/src/components/TournamentTeamOverview.vue
+++ front/src/components/TournamentTeamOverview.vue
@@ -1,30 +1,61 @@
 <template>
 <div class="team">
   <strong>{{name}}</strong>
-  <team-member-list :members="members"></team-member-list>
+  <team-member-list :members="members" :editable="true" @remove="remove">
+  </team-member-list>
   <router-link :to="{ name: 'team', params: { id } }">See more</router-link>
+  <multiselect v-if="tournament.policy.update && !full"
+               :options="potentialPlayers"
+               :searchable="true"
+               label="name"
+               key="name"
+               placeholder="Pick a member"
+               @select="selectMember">
+  </multiselect>
+  <button v-if="policy.join" @click.prevent="$emit('join')">Join</button>
+  <button v-if="policy.leave" @click.prevent="$emit('leave')">Leave</button>
 </div>
 </template>
 
 <script>
 import TeamMemberList from './TeamMemberList'
+import Multiselect from 'vue-multiselect'
+import { mapGetters } from 'vuex'
 
 export default {
-  name: 'TournamentTeam',
+  name: 'TournamentTeamOverview',
   props: {
     id: Number,
     name: String,
     members: Array,
+    numberOfSlots: Number,
+    policy: Object,
+    tournament: Object,
   },
   components: {
     TeamMemberList,
-  },
-  data () {
-    return {
-      editable: false,
-    }
+    Multiselect,
   },
   computed: {
+    full () {
+      return this.numberOfSlots && this.members.length >= this.numberOfSlots
+    },
+    playerIds () {
+      const teamMemberIds = this.tournament.teams.map(t => t.members.map(m => m.id))
+      return new Set([].concat.apply([], teamMemberIds))
+    },
+    potentialPlayers () {
+      return this.userList.filter(user => !this.playerIds.has(user.id))
+    },
+    ...mapGetters(['userList']),
+  },
+  methods: {
+    selectMember(member) {
+      this.$emit('add-member', member)
+    },
+    remove(member) {
+      this.$emit('remove-member', member)
+    },
   },
 }
 </script>
