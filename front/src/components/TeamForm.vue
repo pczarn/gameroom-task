8c5d3744ef28b342c32628414a9fdb0ed3e360e9
Piotr Czarnecki <pioczarn@gmail.com>
2016-10-28 17:40:12 +0200

    Use TeamForm

--- front/src/components/TeamForm.vue
+++ front/src/components/TeamForm.vue
@@ -1,14 +1,27 @@
 <template>
   <div>
-    <form @submit.prevent="$emit('submit')">
+    <form @submit.prevent="submit">
       <fieldset class="row">
         <legend>Name</legend>
         <input type="text" name="name" v-model="team.name">
       </fieldset>
 
-      <fieldset class="row">
+      <fieldset v-if="create" class="row">
         <legend>Members</legend>
-        <!-- <vue-multiselect></vue-multiselect> -->
+        <ul>
+          <li v-for="member in team.members">
+            {{ member.name }}
+            <button @click.prevent="remove(member)">X</button>
+          </li>
+        </ul>
+        <multiselect :options="potentialPlayers"
+                     :searchable="true"
+                     label="name"
+                     key="name"
+                     placeholder="Pick a member"
+                     @select="selectMember"
+                     :reset-after="true">
+        </multiselect>
       </fieldset>
 
       <button type="submit">{{ buttonText }}</button>
@@ -18,19 +31,53 @@
 
 <script>
 import Vue from 'vue'
+import Multiselect from 'vue-multiselect'
 import { mapGetters } from 'vuex'
 
 export default {
   name: 'TeamForm',
+  components: {
+    Multiselect,
+  },
   props: {
-    value: Object,
+    value: {
+      type: Object,
+      default () {
+        return {
+          name: '',
+          members: [],
+        }
+      },
+    },
     buttonText: String,
+    create: Boolean,
   },
   data () {
     return {
       team: Vue.util.extend({}, this.value),
     }
   },
-  computed: mapGetters(['userList']),
+  computed: {
+    potentialPlayers () {
+      const selected = this.team.members.map(m => m.id)
+      return this.userList.filter(user => !selected.includes(user.id))
+    },
+    ...mapGetters(['userList']),
+  },
+  methods: {
+    selectMember (member) {
+      this.team.members.push(member)
+    },
+    remove (member) {
+      const idx = this.team.members.findIndex(m => m.id === member.id)
+      this.team.members.splice(idx, 1)
+    },
+    submit () {
+      this.$emit('submit', this.team)
+      if(this.create) {
+        this.team = Vue.util.extend({}, this.value)
+      }
+    },
+  },
 }
 </script>
--- front/src/components/TeamList.vue
+++ front/src/components/TeamList.vue
@@ -3,20 +3,27 @@
   <div v-for="team in filteredTeams">
     <team-overview v-bind="team"></team-overview>
   </div>
+
+  <team-form button-text="Create team"
+             :create="true"
+             @submit="add">
+  </team-form>
 </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import TeamOverview from './TeamOverview'
+import TeamForm from './TeamForm'
 
 export default {
   name: 'TeamList',
-  props: {
-    user: Object,
-  },
   components: {
     TeamOverview,
+    TeamForm,
+  },
+  props: {
+    user: Object,
   },
   computed: {
     filteredTeams () {
@@ -27,6 +34,11 @@ export default {
       return list
     },
     ...mapGetters(['teamList']),
+  },
+  methods: {
+    add (team) {
+      this.$store.dispatch('CREATE_TEAM', team)
+    },
   },
 }
 </script>
