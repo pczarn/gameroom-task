4e979412b6478c516f16460e9c5757471b6465b3
Piotr Czarnecki <pioczarn@gmail.com>
2016-10-28 15:22:08 +0200

    Add component for editing games

--- front/src/components/Game.vue
+++ front/src/components/Game.vue
@@ -2,18 +2,30 @@
 <div class="game" v-if="game">
   <img :src="game.image_thumb_url">
   <strong>{{ game.name }}</strong>
-  <button v-if="editable" @click="remove">Remove</button>
 
   <router-link :to="{ name: 'game tournaments', params: { id: game.id } }">Tournaments</router-link>
+
   <router-link :to="{ name: 'game matches', params: { id: game.id } }">Matches</router-link>
+
+  <router-link :to="{ name: 'game edit', params: { id: game.id } }" v-if="editable">
+    Edit
+  </router-link>
+
+  <router-link :to="{ name: 'game remove', params: { id: game.id } }" v-if="editable">
+    Remove
+  </router-link>
+
   Statistics
 
-  <router-view :game="game"></router-view>
+  <div>
+    <router-view :game="game"></router-view>
+  </div>
 </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import policies from 'src/policies'
 
 export default {
   name: 'Game',
@@ -25,7 +37,7 @@ export default {
       return this.gameMap.get(this.id)
     },
     editable () {
-      return true
+      return policies.gamePolicy(this.game).update
     },
     ...mapGetters(['gameMap']),
   },
