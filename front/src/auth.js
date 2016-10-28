2223ac188f637b4f80ef94c61f5ef7c4bf65095b
Piotr Czarnecki <pioczarn@gmail.com>
2016-10-28 15:51:17 +0200

    Cleanup auth

--- front/src/auth.js
+++ front/src/auth.js
@@ -21,37 +21,6 @@ export default {
     router.push('/logout')
   },
 
-  // checkAuth() {
-  //   var jwt = localStorage.getItem(SESSION_KEY)
-  //   if(jwt) {
-  //     this.user.authenticated = true
-  //   }
-  //   else {
-  //     this.user.authenticated = false
-  //   }
-  // },
-
-  // // The object to be passed as a header for authenticated requests
-  // getAuthHeader() {
-  //   return {
-  //     'Authorization': 'Bearer ' + localStorage.getItem(SESSION_KEY)
-  //   }
-  // }
-
-  // signup(context, creds, redirect) {
-  //   context.$http.post(SIGNUP_URL, creds, (data) => {
-  //     localStorage.setItem(SESSION_KEY, data.id_token)
-  //     axios.defaults.headers = axios.defaults
-  //     this.user.authenticated = true
-
-  //     if(redirect) {
-  //       router.push(redirect);
-  //     }
-  //   }).error((err) => {
-  //     context.error = err
-  //   })
-  // },
-
   getToken () {
     return localStorage.getItem(SESSION_KEY)
   },
