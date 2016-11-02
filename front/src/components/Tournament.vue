9733592228578ff7fa90a6c939f3d547eef0a996
Piotr Czarnecki <pioczarn@gmail.com>
2016-11-02 14:20:36 +0100

    Refactor tournaments

--- front/src/components/Tournament.vue
+++ front/src/components/Tournament.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="tournament" v-if="rawTournament">
+<div class="tournament" v-if="tournament">
   <a href="#" @click="$router.go(-1)">Go back</a>
 
   <h2>Tournament {{ tournament.title }}</h2>
@@ -15,49 +15,38 @@
 
   <br>
 
-  <router-link v-if="hasRounds && !showRounds" :to="{ name: 'tournament rounds', params: { id } }">Rounds</router-link>
+  <router-link v-if="hasRounds && !showRounds"
+               :to="{ name: 'tournament rounds', params: { id } }">
+    Rounds
+  </router-link>
   <span v-else :class="{ bold: showRounds }">Rounds</span>
 
-  <router-link v-if="!showTeams" :to="{ name: 'tournament teams', params: { id } }">Teams</router-link>
+  <router-link v-if="!showTeams"
+               :to="{ name: 'tournament teams', params: { id } }">
+    Teams
+  </router-link>
   <span v-else :class="{ bold: showTeams }">Teams</span>
 
-  <router-link v-if="canEdit && !showEdit" :to="{ name: 'tournament update', params: { id } }">Edit</router-link>
+  <router-link v-if="canEdit && !showEdit"
+               :to="{ name: 'tournament update', params: { id } }">
+    Edit
+  </router-link>
   <span v-else :class="{ bold: showEdit }">Edit</span>
 
-  <router-link v-if="canDestroy && !showDelete" :to="{ name: 'tournament delete', params: { id } }">Delete</router-link>
+  <router-link v-if="canDestroy && !showDelete"
+               :to="{ name: 'tournament delete', params: { id } }">
+    Delete
+  </router-link>
   <span v-else :class="{ bold: showDelete }">Delete</span>
 
-  <div v-show="showTeams">
-    <h3>Teams</h3>
-    <tournament-team-list :tournament="tournament" :editable="canEdit">
-    </tournament-team-list>
-  </div>
-
-  <div v-show="showRounds">
-    <h3>Rounds</h3>
-    <round v-for="(round, index) in tournament.rounds" :matches="round" :number="index" :tournament="tournament">
-    </round>
-  </div>
-
-  <div v-show="showEdit" v-if="canEdit">
-    Edit tournament
-    <tournament-form button-text="Update tournament"
-                     :value="tournament"
-                     @submit="update">
-    </tournament-form>
-  </div>
-
-  <div v-show="showDelete" v-if="canDestroy">
-    Delete the tournament.
-    <button v-if="canDestroy" @click="destroy()">Delete</button>
-  </div>
+  <router-view :tournament="tournament"></router-view>
 </div>
 </template>
 
 <script>
 import Vue from 'vue'
 import axios from 'axios'
-import { mapGetters, mapActions } from 'vuex'
+import { mapGetters } from 'vuex'
 import policies from 'src/policies'
 import TournamentTeamList from './TournamentTeamList'
 import TournamentForm from './TournamentForm'
@@ -87,26 +76,21 @@ export default {
     id () {
       return parseInt(this.$route.params.id)
     },
-    rawTournament () {
-      return this.tournamentMap.get(this.id)
-    },
     playerIds () {
-      const teamMemberIds = this.rawTournament.teams.map(t => t.members.map(m => m.id))
+      const teamMemberIds = this.tournament.teams.map(t => t.members.map(m => m.id))
       return new Set([].concat.apply([], teamMemberIds))
     },
     tournament () {
-      this.rawTournament.potentialPlayers = this.userList.filter(user => !this.playerIds.has(user.id))
-      this.rawTournament.policy = policies.tournamentPolicy(this.rawTournament)
-      return this.rawTournament
+      return this.tournamentMap.get(this.id)
     },
     showRounds () {
       return this.$route.name === 'tournament rounds' || (
-        this.$route.name == 'tournament' && this.hasRounds
+        this.$route.name === 'tournament' && this.hasRounds
       )
     },
     showTeams () {
       return this.$route.name === 'tournament teams' || (
-        this.$route.name == 'tournament' && !this.hasRounds
+        this.$route.name === 'tournament' && !this.hasRounds
       )
     },
     showEdit () {
@@ -119,26 +103,6 @@ export default {
       return this.tournament.rounds.length > 0
     },
     ...mapGetters(['tournamentMap', 'currentUser', 'isAdmin', 'userList']),
-  },
-  methods: {
-    async update (newTournament) {
-      const newTeams = newTournament.teams.filter(team => team.id === undefined)
-      const promises = newTeams.map(team => this.$store.dispatch('CREATE_TEAM'))
-      const createdTeams = await axios.all(promises)
-      for(const team of createdTeams) {
-        this.tournament.teams.push(team)
-        newTournament.teams.push(team)
-      }
-      this.updateTournament(newTournament)
-    },
-    destroy () {
-      this.destroyTournament(this.tournament)
-      this.$router.push('/tournaments')
-    },
-    ...mapActions({
-      updateTournament: 'UPDATE_TOURNAMENT',
-      destroyTournament: 'DESTROY_TOURNAMENT',
-    }),
   },
 }
 </script>
---
+++ front/src/components/TournamentEdit.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+import { mapActions } from 'vuex'
+import TournamentForm from './TournamentForm'
+
+export default {
+  mixins: [TournamentForm],
+  data () {
+    return {
+      buttonText: 'Update the tournament',
+    }
+  },
+  methods: {
+    async submit () {
+      const newTeams = this.tournamentClone.teams.filter(team => team.id === undefined)
+      const promises = newTeams.map(team => this.$store.dispatch('CREATE_TEAM'))
+      const createdTeams = await axios.all(promises)
+      for(const team of createdTeams) {
+        this.tournamentClone.teams.push(team)
+      }
+      this.updateTournament(this.tournamentClone)
+    },
+    ...mapActions({
+      updateTournament: 'UPDATE_TOURNAMENT',
+    }),
+  },
+}
