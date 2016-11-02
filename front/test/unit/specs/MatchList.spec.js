4fb0add9a166272101c62f28c49d76f396648c9b
Piotr Czarnecki <pioczarn@gmail.com>
2016-11-02 11:46:11 +0100

    Add unit testing

--- front/test/unit/specs/MatchList.spec.js
+++ front/test/unit/specs/MatchList.spec.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import MatchList from 'src/components/MatchList'
+import { store } from 'src/store'
 
 describe('MatchList.vue', () => {
   it('should render correct contents', () => {
     const vm = new Vue({
-      el: document.createElement('div'),
-      render: (h) => h(MatchList)
-    })
-    expect(vm.$el.querySelector('h1').textContent).to.equal('Matches')
+      data () {
+        return {
+          user: null,
+          game: null,
+        }
+      },
+      store,
+      render: h => h(MatchList),
+    }).$mount()
+    expect(vm.$el.querySelector('h1').textContent).to.eq('Matches')
   })
 })
