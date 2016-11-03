c1d1b219daf3a36f57903019fe78ac44010f492b
Piotr Czarnecki <pioczarn@gmail.com>
2016-11-03 13:26:23 +0100

    Add Create and Edit for matches

---
+++ front/src/components/match/Create.vue
@@ -0,0 +1,19 @@
+<script>
+import MatchForm from './Form'
+
+export default {
+  name: 'MatchCreate',
+  mixins: [MatchForm],
+  data () {
+    return {
+      buttonText: 'Create match',
+    }
+  },
+  methods: {
+    submit () {
+      this.$store.dispatch('CREATE_MATCH', this.match)
+      this.match = this.$options.props.value.default()
+    },
+  },
+}
+</script>
---
+++ front/src/components/match/Edit.vue
@@ -0,0 +1,18 @@
+<script>
+import MatchForm from './Form'
+
+export default {
+  name: 'MatchEdit',
+  mixins: [MatchForm],
+  data () {
+    return {
+      buttonText: 'Update match',
+    }
+  },
+  methods: {
+    submit () {
+      this.$store.dispatch('UPDATE_MATCH', this.match)
+    },
+  },
+}
+</script>
