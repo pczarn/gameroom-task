c99440606d208b31f70d092102e41ab43603b8ee
Piotr Czarnecki <pioczarn@gmail.com>
2016-11-03 13:26:10 +0100

    Add Create and Edit for games

---
+++ front/src/components/game/Create.vue
@@ -0,0 +1,19 @@
+<script>
+import GameForm from './Form'
+
+export default {
+  name: 'GameCreate',
+  mixins: [GameForm],
+  data () {
+    return {
+      buttonText: 'Add the game',
+    }
+  },
+  methods: {
+    async submit () {
+      await this.$store.dispatch('CREATE_GAME', this.gameClone)
+      this.gameClone = {}
+    },
+  },
+}
+</script>
