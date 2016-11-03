77b70c00370377619fe45dd40bd1524c1725b292
Piotr Czarnecki <pioczarn@gmail.com>
2016-11-03 16:41:15 +0100

    Fix team form

--- front/src/components/team/Create.vue
+++ front/src/components/team/Create.vue
@@ -1,19 +1,64 @@
+<template>
+<div>
+  <form @submit.prevent="submit">
+    <fieldset class="row">
+      <legend>Name</legend>
+      <input type="text" name="name" v-model="team.name">
+    </fieldset>
+
+    <fieldset class="row">
+      <legend>Members</legend>
+      <ul>
+        <li v-for="member in team.members">
+          {{ member.name }}
+          <button @click.prevent="remove(member)">X</button>
+        </li>
+      </ul>
+      <multiselect :options="potentialPlayers"
+                   :searchable="true"
+                   label="name"
+                   key="name"
+                   placeholder="Pick a member"
+                   @select="selectMember"
+                   :reset-after="true">
+      </multiselect>
+    </fieldset>
+
+    <button type="submit">Create team</button>
+  </form>
+</div>
+</template>
+
 <script>
 import _ from 'lodash'
+import { mapGetters } from 'vuex'
+import Multiselect from 'vue-multiselect'
 import TeamForm from './Form'
 
 export default {
   name: 'TeamCreate',
   mixins: [TeamForm],
-  data () {
-    return {
-      buttonText: 'Create team',
-    }
+  components: {
+    Multiselect,
+  },
+  computed: {
+    potentialPlayers () {
+      const selected = this.team.members.map(m => m.id)
+      return this.userList.filter(user => !selected.includes(user.id))
+    },
+    ...mapGetters(['userList']),
   },
   methods: {
     async submit () {
       await this.$store.dispatch('CREATE_TEAM', this.team)
       this.team = _.cloneDeep(this.blankTeam)
+    },
+    selectMember (member) {
+      this.team.members.push(member)
+    },
+    remove (member) {
+      const idx = this.team.members.findIndex(m => m.id === member.id)
+      this.team.members.splice(idx, 1)
     },
   },
 }
