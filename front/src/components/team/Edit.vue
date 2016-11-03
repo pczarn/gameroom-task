c6155ed93d12c6848e42149a3308ba2d98b9d30c
Piotr Czarnecki <pioczarn@gmail.com>
2016-11-03 16:41:15 +0100

    Fix team form

--- front/src/components/team/Edit.vue
+++ front/src/components/team/Edit.vue
@@ -1,14 +1,22 @@
+<template>
+<div>
+  <form @submit.prevent="submit">
+    <fieldset class="row">
+      <legend>Name</legend>
+      <input type="text" name="name" v-model="team.name">
+    </fieldset>
+
+    <button type="submit">Update team</button>
+  </form>
+</div>
+</template>
+
 <script>
 import TeamForm from './Form'
 
 export default {
   name: 'TeamEdit',
   mixins: [TeamForm],
-  data () {
-    return {
-      buttonText: 'Update team',
-    }
-  },
   methods: {
     submit () {
       this.$store.dispatch('UPDATE_TEAM', this.team)
