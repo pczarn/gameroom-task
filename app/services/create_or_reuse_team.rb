d122c9e8e5a747d435221c1fcaa73012d9a737ca
Piotr Czarnecki <pioczarn@gmail.com>
2016-11-07 19:07:03 +0100

    Fix query for code review

--- app/services/create_or_reuse_team.rb
+++ app/services/create_or_reuse_team.rb
@@ -6,7 +6,9 @@ class CreateOrReuseTeam
   def perform
     team = Team.new(@params)
     member_ids = team.member_ids.sort
-    reused = Team.joins(:users).group(:id).having("array_agg(users.id) = ARRAY[?]", member_ids)[0]
+    reused = Team.joins(:users)
+                 .group(:id)
+                 .having("array_agg(users.id ORDER BY users.id) = ARRAY[?]", member_ids)[0]
     reused || team
   end
 end
